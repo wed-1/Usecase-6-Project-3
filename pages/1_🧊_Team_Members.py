0a75340b4932c545d3de31fecde5493622e53db8
wed-1 <WMwm-050@hotmail.com>
2024-08-10 14:40:35 +0300

    Edit.

--- pages/1_🧊_Team_Members.py
+++ pages/1_🧊_Team_Members.py
@@ -3,11 +3,12 @@ import streamlit as st
 st.set_page_config(page_title="Team Members", page_icon="🧊", layout="wide")
 
 st.markdown("# Team Members")
-# FIXME
+
 st.markdown("""
     - [Wadiah .](https://github.com/wed-1)
-    - 
-    - 
-    - 
-    - 
+    - [Salman .](https://github.com/SalmanGassem)
+    - [Alaa .](https://github.com/alaaAlahmadi2001)
+    - [Raghad .](https://github.com/RaghadAlmalki)
+    - [Abdulaziz .](https://github.com/abdulaziz12121)
+   
 """)
